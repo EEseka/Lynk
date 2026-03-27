c6db5457ce7b9b502704a25d81609148ebd0bc92
Eseka Emmanuel <esekaemmanuel017@gmail.com>
2026-03-27 22:52:08 +0100

    feat: add splash screen UI and app icon

--- androidApp/src/main/kotlin/com/eeseka/androidapp/MainActivity.kt
+++ androidApp/src/main/kotlin/com/eeseka/androidapp/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.compose.ui.tooling.preview.Preview
 import com.eeseka.lynk.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
